349c683f6c988d1b51a418f02419c14ce68f634a
LemonRiz <95.m.rizvi@gmail.com>
2022-12-13 16:41:00 +0000

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import "./App.scss";
 import { React, useState, useEffect } from "react";
 import NavBar from "./containers/NavBar/NavBar";
 import Main from "./containers/Main/Main";
-import beers from "./data";
 
 const App = () => {
   const [beerList, setBeerList] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [filterChoice, setFilterChoice] = useState([]);
   const [numberBeers, setNumberBeers] = useState([25]);
-  // const [acidity, setAcidity] = useState(false);
-
 
   const getBeers = async (numberBeers) => {
     let url = `https://api.punkapi.com/v2/beers?per_page=${numberBeers}`;
